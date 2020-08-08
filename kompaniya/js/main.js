349d92443ed7f96087bc3dcd6d4d59f2aae603d2
dev8953 <69382239+dev8953@users.noreply.github.com>
2020-08-08 22:21:57 +0500

    Add files via upload

---
+++ kompaniya/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+	$('.slider_inner, .news-slider_inner').slick({
+		prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+		nextArrow:'<button type="button" class="slick-btn slick-next"></button>'
+		/*infinite: false*/
+	})
+
+	$('select').styler();
+	
+
+
+})
